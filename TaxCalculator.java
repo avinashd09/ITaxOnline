f88c07597007bb7844b2163cb80b6368289f9b14
avinashd09 <47219665+avinashd09@users.noreply.github.com>
2019-02-07 23:26:19 +0530

    Add files via upload

--- TaxCalculator.java
+++ TaxCalculator.java
@@ -13,10 +13,11 @@ public class TaxCalculator extends HttpServlet {
     
         String s1=req.getParameter("t1");
         String s2=req.getParameter("t2");
-       
+        String s3[]=req.getParameterValues("assets");       
+        String s4=req.getParameter("c1");
         int INCOME=Integer.parseInt(s1);
         int AGE=Integer.parseInt(s2);
-        int tax=0,rebate=0,net_tax=0;
+        int tax=0,rebate=0,surcharge=0,nri_chrg=0,net_tax=0;
        
         if(INCOME>=500000)
         {
@@ -35,11 +36,22 @@ public class TaxCalculator extends HttpServlet {
         rebate=(tax*5)/100;
         }
         net_tax=tax-rebate;
+        if(s3!=null)
+        {
+            surcharge=s3.length*500;
+        }
+        if(s4!=null)
+        {
+        nri_chrg=5000;
+        }
+        
     PrintWriter out=resp.getWriter();
     out.println("THANKS FOR CHOOSING US");
     out.println("YOUR TAXABLE AMT IS " +tax);
     out.println("YOU GOT A REBATE OF " +rebate);
     out.println("YOUR NET TAXABLE AMT IS " +net_tax);
+    out.println("YOUR SURCHARGE AMT IS " +surcharge);
+    out.println("NRI CHARGE " +nri_chrg);
     out.close();
     }    
 }
